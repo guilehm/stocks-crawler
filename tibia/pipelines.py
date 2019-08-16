5d18972c62142a347d6ecd5ec8a53f675b9bc862
Guilherme Luis <guile.hm@hotmail.com>
2019-08-16 19:37:32 -0300

    create pipeline

--- tibia/pipelines.py
+++ tibia/pipelines.py
@@ -4,8 +4,16 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from pymongo import MongoClient
 
 
 class TibiaPipeline(object):
+
+    def __init__(self):
+        self.mongo_client = MongoClient('mongodb://localhost:27017/')
+        self.db = self.mongo_client.tibia
+
     def process_item(self, item, spider):
+        collection = self.db[spider.name]
+        collection.insert_one(dict(item))
         return item
