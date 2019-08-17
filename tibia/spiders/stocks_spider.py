f51b47458086964ea3575dcba773751eca304835
Guilherme Luis <guile.hm@hotmail.com>
2019-08-17 12:25:26 -0300

    create stock spider

--- tibia/spiders/stocks_spider.py
+++ tibia/spiders/stocks_spider.py
@@ -52,11 +52,18 @@ class BaseSpider:
         response = self.session.get(url)
         return Selector(text=response.text)
 
-def extract_from_links(node):
-    return dict(
-        code=node.xpath('text()').get().strip(),
-        url=node.attrib['href'],
-    )
+
+class StockSpider(BaseSpider):
+    def parse_stocks(self, url):
+        response = self.get_response(url)
+        stocks = [extract_from_links(link, 'code') for link in response.xpath('//h2[@class="entry-title"]/a')]
+        names = [name.strip() for name in response.xpath(
+            '//article//div[@class="entry-content entry-summary"]//text()'
+        ).getall() if name and name.strip()]
+        [stock.update(name=name) for stock, name in list(zip(stocks, names))]
+        return stocks
+
+
 def extract_from_links(node, name='name'):
     return {
         name: node.xpath('text()').get().strip(),
