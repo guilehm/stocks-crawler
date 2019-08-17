0be9f76d120b72bda0939f2fb2d8099f1699937d
Guilherme Luis <guile.hm@hotmail.com>
2019-08-17 11:05:41 -0300

    disable pipeline

--- tibia/settings.py
+++ tibia/settings.py
@@ -65,9 +65,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   'tibia.pipelines.TibiaPipeline': 300,
-}
+# ITEM_PIPELINES = {
+#    'tibia.pipelines.TibiaPipeline': 300,
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
