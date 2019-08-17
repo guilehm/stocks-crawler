5a14c4241a04e9d4de49c5b39f70a44820eb53f3
Guilherme Luis <guile.hm@hotmail.com>
2019-08-17 11:05:10 -0300

    change settings

--- tibia/settings.py
+++ tibia/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'tibia.spiders'
 #USER_AGENT = 'tibia (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -34,7 +34,7 @@ ROBOTSTXT_OBEY = True
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
-COOKIES_DEBUG = True
+# COOKIES_DEBUG = True
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
