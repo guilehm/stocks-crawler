b5cdacc7a041ad22e60e2e8947892c14bd456a5e
guilherme <guile.hm@hotmail.com>
2019-08-24 18:08:03 -0300

    create a simple connection to mongo

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+import os
+
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
+
+MONGODB_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017/stocksCrawler')
+
 app = Flask(__name__)
+app.config['MONGO_URI'] = MONGODB_URI
+
+mongo = PyMongo(app)
+db = mongo.db
+stocks_collection = db.stocks_collection
 
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    stocks = [stock for stock in stocks_collection.find()]
+    return jsonify(stocks)
 
 
 if __name__ == '__main__':
