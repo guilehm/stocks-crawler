62702880c3aa87ff33efe701cbf30857c7c15d77
guilherme <guile.hm@hotmail.com>
2019-08-24 13:02:36 -0300

    change save parameter

--- stocks_spider.py
+++ stocks_spider.py
@@ -152,11 +152,11 @@ class StockSpider(BaseSpider):
             self.save_data(output, 'fundamentalistAnalysis')
         return output
 
-    def extract_data_for_all_stocks(self):
+    def extract_data_for_all_stocks(self, save=True):
         stocks_collection = self.db.stocks
         stocks = [stock for stock in stocks_collection.find()]
         for stock in stocks:
-            self.extract_all_fundamentalist_data(stock=stock['code'], save=True, url=stock['url'])
+            self.extract_all_fundamentalist_data(stock=stock['code'], save=save, url=stock['url'])
 
 
 def convert_to_float(value):
