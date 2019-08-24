769b7e5b61c73257f572ee1b8e9b004f23093e11
guilherme <guile.hm@hotmail.com>
2019-08-24 18:57:33 -0300

    set variables

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
+from stocks_spider import StockSpider
 
+
+DEBUG = os.getenv('DEBUG', False)
 MONGODB_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017/stocksCrawler')
+CRAWLER_EMAIL = os.getenv('CRAWLER_EMAIL')
+CRAWLER_PASSWORD = os.getenv('CRAWLER_PASSWORD')
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = MONGODB_URI
 
 mongo = PyMongo(app)
 db = mongo.db
-stocks_collection = db.fundamentalistAnalysis
+stocks_collection = db.stocks
+stocks_analysis_collection = db.stocksAnalysis
 
 
 def convert_id(document):
