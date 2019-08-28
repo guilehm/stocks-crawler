244e4993dad6b9b3d6b58a8f35f2162d1d5064f7
Guilherme Luis <guile.hm@hotmail.com>
2019-08-27 21:29:14 -0300

    fix bug

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ uri_data = MONGODB_URI.rsplit('/', 1)
 db_name = uri_data[-1]
 mongo_url = ''.join(uri_data[:-1])
 SPIDER = StockSpider(
-    CRAWLER_EMAIL, CRAWLER_PASSWORD, mongo_url=mongo_url, db_name=db_name,
+    CRAWLER_EMAIL, CRAWLER_PASSWORD, mongo_url=MONGODB_URI, db_name=db_name,
 )
 
 
