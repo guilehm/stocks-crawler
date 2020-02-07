5cd94d197464aee10f3eca74fbc05c598da737e0
guilherme <guile.hm@hotmail.com>
2020-02-07 11:48:13 -0300

    start twitter crawler

---
+++ valor_investe/twitter_crawler.py
@@ -0,0 +1,28 @@
+import logging
+import os
+import sys
+
+import requests
+from scrapy.selector import Selector
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.Formatter('%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s')
+URL = os.getenv('VALOR_TWITTER_URL', 'https://twitter.com/valorinveste/')
+
+
+class TwitterCrawler:
+    def __init__(self, url=URL):
+        self.url = url
+        self.response = None
+
+    def get_response(self):
+        response = requests.get(self.url)
+        try:
+            response.raise_for_status()
+            self.response = Selector(text=response.text)
+        except requests.RequestException:
+            logging.exception(
+                'Could not request to Valor Invest Twitter URL',
+                exc_info=True,
+            )
+        return self.response
