a56191211c4133e1738e7ec5c31a012358c62b7a
guilherme <guile.hm@hotmail.com>
2020-02-22 15:03:07 -0300

    improve driver

--- selenium/driver.py
+++ selenium/driver.py
@@ -1,14 +1,32 @@
+
 import os
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 CHROME_DRIVER_LOCATION = os.getenv('CHROME_DRIVER_LOCATION')
 
 
 class Driver:
 
-    def get_driver(self, location=CHROME_DRIVER_LOCATION):
-        if location:
-            return webdriver.Chrome(location)
-        return webdriver.Chrome()
+    def __init__(self, location=CHROME_DRIVER_LOCATION, headless=True, user_agent=''):
+        self.location = location
+        self.headless = headless
+        self.user_agent = user_agent
 
+    def _get_options(self):
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        if self.user_agent:
+            chrome_options.add_argument(f'user-agent={self.user_agent}')
+        if self.headless:
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--disable-gpu')
+        return chrome_options
+
+    def get_driver(self):
+        params = dict(options=self._get_options())
+        if self.location:
+            params.update(drive_location=self.location)
+        return webdriver.Chrome(**params)
