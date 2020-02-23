600069c4a9967b497295485e24c02b2f3926e738
guilherme <guile.hm@hotmail.com>
2020-02-23 02:15:42 -0300

    improve builder

--- driver_builder/builder.py
+++ driver_builder/builder.py
@@ -3,15 +3,21 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-DRIVER_NAME = os.getenv('DRIVER_NAME', 'chromedriver73')
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
              'Chrome/60.0.3112.50 Safari/537.36'
+DRIVER_NAME = os.getenv('DRIVER_NAME', 'chromedriver73')
+CHROMEDRIVER_PATH = os.getenv('CHROMEDRIVER_PATH')
+GOOGLE_CHROME_BIN = os.getenv('GOOGLE_CHROME_BIN')
 
 
 class Driver:
 
-    def __init__(self, driver_name=DRIVER_NAME, headless=True, user_agent=USER_AGENT):
-        self.location = f'{os.getcwd()}/driver_builder/chromedriver/{driver_name}'
+    @staticmethod
+    def __get_location():
+        return CHROMEDRIVER_PATH or f'{os.getcwd()}/driver_builder/chromedriver/{DRIVER_NAME}'
+
+    def __init__(self, headless=True, user_agent=USER_AGENT):
+        self.location = self.__get_location()
         self.headless = headless
         self.user_agent = user_agent
 
@@ -20,6 +26,8 @@ class Driver:
         chrome_options.add_argument('--no-sandbox')
         if self.user_agent:
             chrome_options.add_argument(f'user-agent={self.user_agent}')
+        if GOOGLE_CHROME_BIN:
+            chrome_options.binary_location = GOOGLE_CHROME_BIN
         if self.headless:
             chrome_options.add_argument('--headless')
             chrome_options.add_argument('--disable-gpu')
